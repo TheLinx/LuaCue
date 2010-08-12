5a3545a21b53d0f05f6636935a84704294220f93
Linus Sjögren <thelinx@unreliablepollution.net>
2010-08-12 05:26:46 +0200

    First release.
    This is the first release of LuaCue 1.0.0. It has decoding
    functionality, ready to be used and abused. Thanks to the
    "Wikipedia article on cuesheets":[1] as provided an
    easy-to-understand documentation of the cuesheet format.
    Also, thanks to "lhf":[2] and "jA_cOp":[3] for helping me out in
    the Lua IRC channel.
    List of features:
    
      * Cuesheet decoding.
      * Basic UTF-8 compliance (it "will work" with UTF-8 input files)
    
    [1]:http://en.wikipedia.org/wiki/Cue_sheet_(computing)
    [2]:http://www.tecgraf.puc-rio.br/~lhf/
    [3]:http://github.com/JakobOvrum

---
+++ examples/unicode.lua
@@ -0,0 +1,53 @@
+pcall(require, "printr")
+package.path = "../?/init.lua;"..package.path
+require("cue")
+
+-- from my music folder...
+sheet = [[﻿REM GENRE Game
+REM DATE 2010
+REM DISCID 49062507
+REM COMMENT "ExactAudioCopy v0.99pb5"
+PERFORMER "イオシス"
+TITLE "東方アゲハ"
+FILE "東方アゲハ.tta" WAVE
+  TRACK 01 AUDIO
+    TITLE "Letty Whiterock You!"
+    PERFORMER "miko"
+    INDEX 01 00:00:00
+  TRACK 02 AUDIO
+    TITLE "キャプテン・ムラサのケツアンカー"
+    PERFORMER "山本 椛"
+    INDEX 01 02:20:49
+  TRACK 03 AUDIO
+    TITLE "断罪ヤマザナドゥ！"
+    PERFORMER "岩杉 夏"
+    INDEX 01 05:44:01
+  TRACK 04 AUDIO
+    TITLE "てんこの恋愛下克上！エクスクラクラ☆ラメーション！"
+    PERFORMER "miko"
+    INDEX 01 09:39:39
+  TRACK 05 AUDIO
+    TITLE "水橋ジェラシックパーク"
+    PERFORMER "miko"
+    INDEX 01 12:48:26
+  TRACK 06 AUDIO
+    TITLE "ひなりんのヤクい関係"
+    PERFORMER "一ノ瀬 月琉"
+    INDEX 01 18:41:50
+  TRACK 07 AUDIO
+    TITLE "キャプテン・ARMのケツアンカー"
+    PERFORMER "ARM"
+    INDEX 01 22:53:18]]
+
+decoded = cue.decode(sheet)
+
+if printr then
+  printr(decoded)
+else
+  print("if you had a printr module, you'd see a pretty table right here")
+end
+
+print""
+
+print("title of track 3: "..decoded.tracks[3].title)
+print("start-index of track 7: "..decoded.tracks[7].indices[1])
